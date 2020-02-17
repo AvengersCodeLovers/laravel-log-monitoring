fd299537976cfb2abc5c36e421ebf49fc0382952
huusu1996@gmail.com <huusu1996@gmail.com>
2020-02-17 14:57:41 +0700

    Innit project

---
+++ src/MonitoringServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Sun\Auth;
+
+use Illuminate\Support\ServiceProvider;
+
+class MonitoringServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+}
