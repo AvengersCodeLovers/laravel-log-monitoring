31a52b6d05316d825f3143c920c3fcc7aada08bf
thangcx-0794 <chu.xuan.thang@sun-asterisk.com>
2020-06-16 10:56:32 +0700

    New message template

--- README.md
+++ README.md
@@ -25,6 +25,24 @@ If you don't use auto-discovery, add the ServiceProvider to the providers array 
 ],
 ```
 
+Set your session start to global middleware in app/Http/Kernel.php
+```php
+protected $middleware => [
+    // Other global middleware...
+
+    \Illuminate\Session\Middleware\StartSession::class,
+];
+
+protected $middlewareGroups => [
+    'web' => [
+        \App\Http\Middleware\EncryptCookies::class,
+        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        //\Illuminate\Session\Middleware\StartSession::class,       // Comment this
+        // ...
+    ],
+];
+```
+
 You will also need to add api_key chatwork bot and room sos for services your application utilizes. These credentials should be placed in your `config/services.php` configuration file, and use the key `chatwork`. For example:
 
 ```php
@@ -48,7 +66,7 @@ CHATWORK_API_ROOM_ID=xxxxx
 
 Add Monitoring Log Laravel reporting to App/Exceptions/Handler.php:
 
-```
+```php
 public function report(Exception $exception)
 {
     app('monitoring')->sendExceptionToChatWork($exception);
@@ -60,15 +78,37 @@ public function report(Exception $exception)
 ### Basic Usage Monitoring Log Queue Error
 Follow document in https://laravel.com/docs/master/queues#cleaning-up-after-failed-jobs
 
+```php
+/**
+ * The job failed to process.
+ *
+ * @param  Exception  $exception
+ * @return void
+ */
+public function failed(Exception $exception)
+{
+    app('monitoring')->sendExceptionToChatWork($exception);
+}
 ```
-    /**
-     * The job failed to process.
-     *
-     * @param  Exception  $exception
-     * @return void
-     */
-    public function failed(Exception $exception)
-    {
-        app('monitoring')->sendExceptionToChatWork($exception);
-    }
+
+### Advance Usage
+
+Monitoring Log Laravel support reporting accept `$request` as param with supportable functions which come with a message with more infomation
+
+Add Monitoring Log Laravel reporting to App/Exceptions/Handler.php:
+
+```php
+/**
+ * Render an exception into an HTTP response.
+ *
+ * @param  \Illuminate\Http\Request  $request
+ * @param  \Exception  $exception
+ * @return \Illuminate\Http\Response
+ */
+public function render($request, Exception $exception)
+{
+    app('monitoring')->sendExceptionToChatWork($exception, $request);
+
+    return parent::render($request, $exception);
+}
 ```
--- src/AbstractHandler.php
+++ src/AbstractHandler.php
@@ -3,12 +3,17 @@
 namespace AvengersGroup;
 
 use Exception;
+use Illuminate\Support\Facades\Auth;
 use wataridori\ChatworkSDK\ChatworkSDK;
 use wataridori\ChatworkSDK\ChatworkRoom;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Session\TokenMismatchException;
+use Illuminate\Http\Exceptions\ThrottleRequestsException;
 use wataridori\ChatworkSDK\Exception\RequestFailException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 class AbstractHandler
 {
@@ -19,14 +24,19 @@ class AbstractHandler
      *
      * @return mixed
      */
-    public function sendExceptionToChatWork($exception)
+    public function sendExceptionToChatWork($exception, $request = null)
     {
         try {
             // If have more case should use switch case
-            if ($exception instanceof ValidationException || $exception instanceof AuthenticationException) {
+            if ($exception instanceof ValidationException
+                || $exception instanceof AuthenticationException
+                || $exception instanceof NotFoundHttpException
+                || $exception instanceof TokenMismatchException
+                || $exception instanceof MethodNotAllowedHttpException
+                || $exception instanceof ThrottleRequestsException) {
                 return false;
             }
-            $message = $this->getTemplateMessage($exception);
+            $message = $this->getTemplateMessage($exception, $request);
             ChatworkSDK::setApiKey(config('services.chatwork.api_key'));
             $room = new ChatworkRoom(config('services.chatwork.room_id_sos'));
             $members = $room->getMembers();
@@ -47,15 +57,32 @@ class AbstractHandler
     /**
      * Get Template Message Exception
      *
-     * @param \Exception $e
+     * @param  \Exception  $e
+     * @param  null|\Illuminate\Http\Request  $r
      *
      * @return string
      */
-    protected function getTemplateMessage($e)
+    protected function getTemplateMessage($e, $r)
     {
-        return '[info][title]Bug in ' . env('APP_ENV') . '[/title][code]Message: ' . $e->getMessage()
-        . ' in file ' . $e->getFile()
-        . ' line ' . $e->getLine()
-        . '[/code][/info]';
+        $uid = Auth::check() ? Auth::user()->id : 'N/A';
+        $message = $e->getMessage() ? $e->getMessage() : 'N/A';
+
+        if ($r) {
+            return '[info][title]Bug in ' . env('APP_ENV') . '[/title][code]Message: ' . $message . '
+            In file: ' . $e->getFile() . '
+            Line: ' . $e->getLine() . '
+            Request uri: ' . $r->getRequestUri() . '
+            Method: ' . $r->getMethod() . '
+            Previous uri: ' . str_replace(url('/'), '', url()->previous()) . '
+            User agent: ' . $r->header('User-Agent') . '
+            User id: ' . $uid
+            . ' [/code][/info]';
+        }
+
+        return '[info][title]Bug in ' . env('APP_ENV') . '[/title][code]Message: ' . $message . '
+        In file: ' . $e->getFile() . '
+        Line: ' . $e->getLine() . '
+        User id: ' . $uid
+        . ' [/code][/info]';
     }
 }
